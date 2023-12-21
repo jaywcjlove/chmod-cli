180d07d3833e1c781a88cbef05e6689ce9fcc98f
jaywcjlove <398188662@qq.com>
2023-12-21 13:26:23 +0800

    type: fix type issue.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,7 @@
-
-function chmod(path: string,mode: string | number, callback?: (err: Error | null) => void): void;
-
-export default chmod;
+/**
+ * Changing file permissions
+ * @param {import('fs').PathLike} path
+ * @param {string|number} mode
+ * @param {(err: Error | null) => void} callback
+ */
+export default function chmod(path: import('fs').PathLike, mode: string | number, callback: (err: Error | null) => void): void;
